bce27048ec4ee5ede8739d1c49ad6831e8ab22f4
Matheus Patricio <patriciomtt@outlook.com>
2025-03-07 12:23:19 -0300

    JSI - Desenvolvimento - MD3

---
+++ js/JSI - desenvolvimento MD3/script.js
@@ -0,0 +1,7 @@
+function imprimeMensagem() {
+    console.log("Conexão feita com sucesso!")
+    console.log("Conexão feita com sucesso!")
+    console.log("Conexão feita com sucesso!")
+}
+
+imprimeMensagem()
